82fa70119368a3bd1459e4ea7116d09bfa5f73bd
Peng <kingalagon23@gmail.com>
2024-12-03 15:24:53 +0800

    changes on the contact page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -26,45 +26,69 @@ function Contact() {
 
             {/* Contact Details Grid */}
             <div className="grid grid-cols-1 md:grid-cols-3 gap-12 max-w-4xl w-full">
-                {/* Email */}
-                <div className="relative bg-gray-700 p-6 rounded-lg shadow-lg overflow-hidden group transform transition-transform duration-300 hover:scale-105">
-                    <div className="flex flex-col items-center text-center p-6 transition-opacity duration-300 ease-in-out opacity-100 group-hover:opacity-0">
+                <div className="relative bg-gray-700 p-6 rounded-lg shadow-lg overflow-hidden group transform md:transition-transform md:duration-300 md:hover:scale-105">
+                    <div className="flex flex-col items-center text-center p-6 md:hidden">
                         <FaEnvelope className="text-yellow-500 text-4xl mb-4" />
-                        <h3 className="text-xl font-bold">Email</h3>
-                    </div>
-                    <div className="absolute inset-0 p-6 flex flex-col justify-center items-center text-white opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-in-out">
                         <h3 className="text-xl font-bold">Email</h3>
                         <a href="mailto:kingalagon23@gmail.com" className="hover:underline text-gray-400 mt-2">
                             kingalagon23@gmail.com
                         </a>
                     </div>
+                    <div className="hidden md:block">
+                        <div className="flex flex-col items-center text-center p-6 transition-opacity duration-300 ease-in-out opacity-100 group-hover:opacity-0">
+                            <FaEnvelope className="text-yellow-500 text-4xl mb-4" />
+                            <h3 className="text-xl font-bold">Email</h3>
+                        </div>
+                        <div className="absolute inset-0 p-6 flex flex-col justify-center items-center text-white opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-in-out">
+                            <h3 className="text-xl font-bold">Email</h3>
+                            <a href="mailto:kingalagon23@gmail.com" className="hover:underline text-gray-400 mt-2">
+                                kingalagon23@gmail.com
+                            </a>
+                        </div>
+                    </div>
                 </div>
 
                 {/* Phone */}
-                <div className="relative bg-gray-700 p-6 rounded-lg shadow-lg overflow-hidden group transform transition-transform duration-300 hover:scale-105">
-                    <div className="flex flex-col items-center text-center p-6 transition-opacity duration-300 ease-in-out opacity-100 group-hover:opacity-0">
+                <div className="relative bg-gray-700 p-6 rounded-lg shadow-lg overflow-hidden group transform md:transition-transform md:duration-300 md:hover:scale-105">
+                    <div className="flex flex-col items-center text-center p-6 md:hidden">
                         <FaPhone className="text-yellow-500 text-4xl mb-4" />
-                        <h3 className="text-xl font-bold">Phone</h3>
-                    </div>
-                    <div className="absolute inset-0 p-6 flex flex-col justify-center items-center text-white opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-in-out">
                         <h3 className="text-xl font-bold">Phone</h3>
                         <a href="tel:+639053183427" className="hover:underline text-gray-400 mt-2">
                             +63 905 318 3427
                         </a>
                     </div>
+                    <div className="hidden md:block">
+                        <div className="flex flex-col items-center text-center p-6 transition-opacity duration-300 ease-in-out opacity-100 group-hover:opacity-0">
+                            <FaPhone className="text-yellow-500 text-4xl mb-4" />
+                            <h3 className="text-xl font-bold">Phone</h3>
+                        </div>
+                        <div className="absolute inset-0 p-6 flex flex-col justify-center items-center text-white opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-in-out">
+                            <h3 className="text-xl font-bold">Phone</h3>
+                            <a href="tel:+639053183427" className="hover:underline text-gray-400 mt-2">
+                                +63 905 318 3427
+                            </a>
+                        </div>
+                    </div>
                 </div>
-
-                {/* Address */}
-                <div className="relative bg-gray-700 p-6 rounded-lg shadow-lg overflow-hidden group transform transition-transform duration-300 hover:scale-105">
-                    <div className="flex flex-col items-center text-center p-6 transition-opacity duration-300 ease-in-out opacity-100 group-hover:opacity-0">
+                <div className="relative bg-gray-700 p-6 rounded-lg shadow-lg overflow-hidden group transform md:transition-transform md:duration-300 md:hover:scale-105">
+                    <div className="flex flex-col items-center text-center p-6 md:hidden">
                         <FaMapMarkerAlt className="text-yellow-500 text-4xl mb-4" />
                         <h3 className="text-xl font-bold">Address</h3>
-                    </div>
-                    <div className="absolute inset-0 p-6 flex flex-col justify-center items-center text-white opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-in-out">
-                        <h3 className="text-xl font-bold">Address</h3>
-                        <p className="text-gray-400 mt-2 ">
+                        <p className="text-gray-400 mt-2 text-center">
                             Blk 26 Lot 8 Laguna Lake St. Gulfview Bago Aplaya, Davao City
                         </p>
+                    </div>
+                    <div className="hidden md:block">
+                        <div className="flex flex-col items-center text-center p-6 transition-opacity duration-300 ease-in-out opacity-100 group-hover:opacity-0">
+                            <FaMapMarkerAlt className="text-yellow-500 text-4xl mb-4" />
+                            <h3 className="text-xl font-bold">Address</h3>
+                        </div>
+                        <div className="absolute inset-0 p-6 flex flex-col justify-center items-center text-white opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-in-out">
+                            <h3 className="text-xl font-bold">Address</h3>
+                            <p className="text-gray-400 mt-2 text-center">
+                                Blk 26 Lot 8 Laguna Lake St. Gulfview Bago Aplaya, Davao City
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
